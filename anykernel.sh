a5aa35b4ac066d8be239472d1b0470ab9bdfb289
dewy1p1 <118836143+dewy1p1@users.noreply.github.com>
2023-06-23 19:55:19 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -10,11 +10,8 @@ do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=selene
-device.name2=selenes
-device.name3=
-device.name4=
-device.name5=
+device.name1=mona
+device.name2=monas
 supported.versions=
 supported.patchlevels=
 '; } # end properties
